0887aa4316d0eec96e684d90ce0043c6938008c7
Gustavo <gustavoalves003.dev@gmail.com>
2025-11-20 21:03:08 -0300

    refactor: remove comments and emojis from global type definitions

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="bun-types" />
 
 declare global {
-  // Bun já tem console e process globalmente disponíveis
-  // Este arquivo garante que o TypeScript reconheça essas APIs
   var console: Console;
   var process: NodeJS.Process;
 }
