f711ec12fc4bfeede9873f6e05a94c78e851b6cc
Gustavo <gustavoalves003.dev@gmail.com>
2025-11-20 21:21:46 -0300

    feat(presentation): add message handler

---
+++ src/presentation/handlers/message.handler.ts
@@ -0,0 +1,25 @@
+import { Message } from "@domain/entities/message.entity";
+import { ProcessMessageUseCase } from "@application/use-cases/process-message.use-case";
+import { SendMessageUseCase } from "@application/use-cases/send-message.use-case";
+import { SendMessageDto } from "@application/dtos/message.dto";
+
+export class MessageHandler {
+  constructor(
+    private readonly processMessageUseCase: ProcessMessageUseCase,
+    private readonly sendMessageUseCase: SendMessageUseCase
+  ) {}
+
+  async handle(message: Message): Promise<void> {
+    const result = await this.processMessageUseCase.execute(message);
+
+    if (result.success) {
+      const sendDto: SendMessageDto = {
+        to: message.from,
+        message: result.data,
+      };
+
+      await this.sendMessageUseCase.execute(sendDto);
+    }
+  }
+}
+
